97ba9bcd304bd8fe1e74bd47b9751459b96f8d4f
Juraj <juraj.mader21@gmail.com>
2026-07-06 15:44:43 +0200

    Refactor POM artifact naming and add Customer entity implementation

--- booking-service/src/main/java/com/example/bookingservice/service/BookingService.java
+++ booking-service/src/main/java/com/example/bookingservice/service/BookingService.java
@@ -1,13 +1,27 @@
 package com.example.bookingservice.service;
 
+import com.example.bookingservice.Entity.Customer;
+import com.example.bookingservice.repository.CustomerRepository;
 import com.example.bookingservice.request.BookingRequest;
 import com.example.bookingservice.response.BookingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingService {
 
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public BookingService(final CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public BookingResponse createBooking(final BookingRequest request) {
+
+        Customer customer = customerRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+
         return BookingResponse.builder().build();
 
     }
