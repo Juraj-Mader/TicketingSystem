db96ca9d4d740232b23e238dc9857933e99ae3ba
Juraj <juraj.mader21@gmail.com>
2026-07-06 14:34:17 +0200

    Add customer and booking creation logic with Optional.orElseThrow

---
+++ booking-service/src/main/java/com/example/bookingservice/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.bookingservice.repository;
+
+import com.example.bookingservice.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
--- booking-service/src/main/java/com/example/bookingservice/service/BookingService.java
+++ booking-service/src/main/java/com/example/bookingservice/service/BookingService.java
@@ -1,13 +1,34 @@
 package com.example.bookingservice.service;
 
+import com.example.bookingservice.Entity.Customer;
+import com.example.bookingservice.repository.CustomerRepository;
 import com.example.bookingservice.request.BookingRequest;
 import com.example.bookingservice.response.BookingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingService {
 
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public BookingService(final CustomerRepository customerRepository)  {
+        this.customerRepository = customerRepository;
+    }
+
     public BookingResponse createBooking(final BookingRequest request) {
+
+        /**
+         final Customer customer = customerRepository.findById(request.getUserId()).orElse(null);
+        if (customer == null) {
+            throw new RuntimeException("Customer not found");
+        }
+         */
+        Customer customer = customerRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+
         return BookingResponse.builder().build();
 
     }
