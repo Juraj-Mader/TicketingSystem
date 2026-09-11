ddcb8c82ac0fd2da86c98da813089587e80746ac
Juraj <juraj.mader21@gmail.com>
2026-09-11 20:52:02 +0200

    + OrderRepository

--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -2,7 +2,9 @@ package com.example.order_service.service;
 
 import com.example.bookingservice.event.BookingEvent;
 import com.example.order_service.entity.Order;
+import com.example.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class OrderService {
 
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @KafkaListener(topics = "booking", groupId = "order-service")
     public void orderEvent(BookingEvent bookingEvent) {
         log.info("Order Event received: {}", bookingEvent);
 
         Order order = createOrder(bookingEvent);
+        orderRepository.saveAndFlush(order);
     }
 
     private Order createOrder(BookingEvent bookingEvent) {
